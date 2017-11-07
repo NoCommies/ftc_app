1f9b92b28d83d16750fe97ef55fee35743e0a73d
FTC4924 <nrrobotics@gmail.com>
2017-11-07 17:58:10 -0500

    This is the final teleOp program for the 11/11/17 competition. This can control the linear slide, elbow, and claw. It has new comments.
    -Ayca

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_Holonomic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_Holonomic.java
@@ -86,7 +86,10 @@ public class RR_Holonomic extends OpMode {
         collectionMotor.setDirection(DcMotor.Direction.FORWARD);
         relicExtension.setDirection(DcMotor.Direction.FORWARD);
         deliveryMotor.setDirection(DcMotor.Direction.FORWARD);
+        //Set the 180 servos to their middle position
         barServo.setPosition(MIDDLEPOSITION180);
+        //Set the continous servos to a neutral power, to make sure they do not move while
+        // initiallizing
         clawServo.setPower(0);
         elbowServo.setPower(0);
         // Tell the driver that initialization is complete.
@@ -113,6 +116,7 @@ public class RR_Holonomic extends OpMode {
      */
     @Override
     public void loop() {
+        //We initialize and give certain values to certain variables, so that we can use them later
         double collectionPower = 0.0;
         double deliveryPower = 0.0;
         final double BARMOVE = -1.0;
@@ -120,7 +124,8 @@ public class RR_Holonomic extends OpMode {
         double position = 0.0;
         double clawPosition = 0.0;
 
-        //we set what to do when the motor is not given power, which is to brake completely, instead of coasting
+        //we set what to do when the motor is not given power, which is to brake completely,
+        //instead of coasting
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -128,7 +133,7 @@ public class RR_Holonomic extends OpMode {
 
 
         double drive = -gamepad1.left_stick_y;
-        //drive is what direction we want to move, either forwards, backwards, or neither
+        //drive is what direction we want to    move, either forwards, backwards, or neither
         double holonomic = -gamepad1.left_stick_x;
         //holonomic is what direction we want to move sideways
         double turnRight = gamepad1.right_trigger;
@@ -138,16 +143,22 @@ public class RR_Holonomic extends OpMode {
         boolean collectionPowerUp = gamepad2.b;
         //collectionPowerUp is dependent on whether or not we want the collection to collect
         boolean collectionPowerDown = gamepad2.a;
-        //collectionPowerDown is dependent on whether or not we want the collection deliver (Push downwards)
+        //collectionPowerDown is dependent on whether or not we want the collection deliver
+        //(Push downwards)
         boolean deliveryUp = gamepad2.dpad_up;
         //deliveryUp is dependent on whether or not we want the delivery to deliver
         boolean deliveryDown = gamepad2.dpad_down;
         //deliveryDown is dependent on whether or not we want the delivery to go downwards
         double linearSlide =  (gamepad2.left_stick_y);
          //this extends the linearSlide using the measuring tape to extend it
-        boolean halfSpeed = gamepad1.left_bumper;
+        boolean slowSpeed = gamepad1.left_bumper;
+        //slowSpeed says whether or not the driver wants to go at a slower speed, which can cause
+        // finer adjustments
         double elbow = gamepad1.right_stick_y;
+        //elbow is a variable set to the amount of power the driver wants to give to move this servo
         double clawPower = gamepad2.right_stick_y;
+        //clawPower is a variable set to the amount of power the driver wants to give to move
+        // this servo
 
         if (collectionPowerUp) {
             //if we want it to collect, we set collectionPower to 1
@@ -158,33 +169,47 @@ public class RR_Holonomic extends OpMode {
         }
 
         if (gamepad2.left_bumper) {
+            //if the driver hits the left bumper, that signals that the driver wants to make
+            // the kicker go up, and kick the block up
             barServo.setPosition(BARDOWN + BARMOVE);
             position = BARDOWN + BARMOVE;
         } else {
+            //If the driver does not press this button, we let the kicker fall down
             barServo.setPosition(BARDOWN);
             position = BARDOWN;
         }
 
         if (deliveryUp) {
+            //if we want the delivery to move the glyph forwards/deliver the glyph,
+            //we set deliveryPower to -1
             deliveryPower = -1;
         } else if (deliveryDown) {
-            //if we want the collection to deliver/spin backswards, we set collectionPower to -1
+            //if we want the delivery to move the glyph backwards,
+            //we set deliveryPower to 1
             deliveryPower = 1;
         }
 
-        //we are calculating the power to send to each different wheel, which each need their own power since it is calculated in different ways
+        //we calculate the power to send to each different wheel, which each need their own power
+        //since it is calculated in different ways, because of the turning and holonomic abilities
 
-        double frontLeftPower =  Range.clip(drive - holonomic + turnRight - turnLeft, -1.0, 1.0);
-        double frontRightPower =  Range.clip(drive + holonomic - turnRight + turnLeft, -1.0, 1.0);
-        double backRightPower =  Range.clip(drive - holonomic - turnRight + turnLeft, -1.0, 1.0);
-        double backLeftPower =  Range.clip(drive + holonomic + turnRight - turnLeft, -1.0, 1.0);
+        double frontLeftPower =
+                Range.clip(drive - holonomic + turnRight - turnLeft, -1.0, 1.0);
+        double frontRightPower =
+                Range.clip(drive + holonomic - turnRight + turnLeft, -1.0, 1.0);
+        double backRightPower =
+                Range.clip(drive - holonomic - turnRight + turnLeft, -1.0, 1.0);
+        double backLeftPower =
+                Range.clip(drive + holonomic + turnRight - turnLeft, -1.0, 1.0);
+        //We also clip the values, so when we calculate the slowSpeed values,
+        //it does not turn the value 1.5 (which would be clipped automatically by the motor) to 0.3,
+        //which is not a fifth of what the power of the motor would be
 
-        if (halfSpeed) {
+        if (slowSpeed) {
+            //to calculate the slower speed, we multiply the prevously calculated powers by 0.2
             frontLeftPower = 0.2 * (frontLeftPower);
             frontRightPower = 0.2 * (frontRightPower);
             backRightPower = 0.2 * (backRightPower);
             backLeftPower = 0.2 * (backLeftPower);
-
         }
 
         // Send calculated power to wheels and motors
@@ -200,11 +225,9 @@ public class RR_Holonomic extends OpMode {
 
 
         // Show the elapsed game time
-       // telemetry.addData("Status", "Run Time: " + runtime.toString());b
-        telemetry.addData("Slow Mode", halfSpeed);
-        telemetry.addData("Bar Servo", "Position" + position);
-        telemetry.addData("Claw Servo", "Position" + clawPosition);
+       telemetry.addData("Status", "Run Time: " + runtime.toString());
 
+       //we make the turn values 0, so that the robot will stop turning
         turnLeft = 0;
         turnRight = 0;
 
@@ -215,6 +238,16 @@ public class RR_Holonomic extends OpMode {
      */
     @Override
     public void stop() {
+        //We make sure to turn everything off when the driver hits STOP
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        collectionMotor.setPower(0);
+        relicExtension.setPower(0);
+        deliveryMotor.setPower(0);
+        elbowServo.setPower(0);
+        clawServo.setPower(0);
     }
 
 }
